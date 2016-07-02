44b14fc9102f4144a89538a9ac5121092014088d
David Bjorklund <david.bjorklund@gmail.com>
2016-07-02 19:10:42 +0200

    support simple nested packages

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 import 'babel-polyfill';
 import test from 'tapava';
 import getIdealPackageTree from './lib';
+import assert from 'assert';
 
 const shouldNotBeCalled = () => { throw new Error('should not be called'); };
-const setupGetPackage = obj => key => Promise.resolve(obj[key]);
+const setupGetPackage = obj => key => {
+  assert(obj[key], `${key} exists in ${JSON.stringify(obj)}`);
+  return Promise.resolve(obj[key]);
+};
 
 test('empty array', function * (t) {
   const actual = yield getIdealPackageTree(shouldNotBeCalled)([]);
-  const expected = [];
+  const expected = {};
   t.deepEqual(actual, expected);
 });
 
@@ -20,20 +24,35 @@ test('packages w no dependencies', function * (t) {
   const actual = yield getIdealPackageTree(getPackage)([
     'foo', '@scope/bas@1.2.0', 'bar@latest'
   ]);
-  const expected = [
-    {
-      name: '@scope/bas',
-      version: '1.2.0',
-      dependencies: []
-    }, {
-      name: 'bar',
-      version: '3.0.0',
-      dependencies: []
-    }, {
-      name: 'foo',
-      version: '1.0.0',
-      dependencies: []
+  const expected = {
+    '@scope/bas': {
+      version: '1.2.0'
+    },
+    bar: {
+      version: '3.0.0'
+    },
+    foo: {
+      version: '1.0.0'
     }
-  ];
+  };
+  t.deepEqual(actual, expected);
+});
+
+test('packages w nested dependencies that are all unique', function * (t) {
+  const getPackage = setupGetPackage(Object.freeze({
+    foo: {name: 'foo', version: '1.0.0', dependencies: {
+      bar: '^2.0.0'
+    }},
+    'bar@^2.0.0': {name: 'bar', version: '2.1.2', dependencies: {
+      bas: '~1.0.0'
+    }},
+    'bas@~1.0.0': {name: 'bas', version: '1.0.5'}
+  }));
+  const actual = yield getIdealPackageTree(getPackage)(['foo']);
+  const expected = {
+    foo: {version: '1.0.0'},
+    bar: {version: '2.1.2'},
+    bas: {version: '1.0.5'}
+  };
   t.deepEqual(actual, expected);
 });
