6fb75d1dd4e55c34a98af86948339ab7adfc5db6
David Bjorklund <david.bjorklund@gmail.com>
2016-07-08 22:13:19 +0200

    fix cli

--- bin/cli.js
+++ bin/cli.js
@@ -2,8 +2,6 @@
 
 var args = process.argv.slice(2);
 
-var getPackage = require('get-package-json-from-registry')();
-
-require('../dist')(getPackage)(args)
+require('../dist')(args)
   .then(json => console.log(JSON.stringify(json, null, 2)))
   .catch(err => console.error(err));
